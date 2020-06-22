81a87502b2fd5391eeb7faab892261479ffefefa
Mitul Gautam <mitulgautam@outlook.com>
2020-06-22 16:37:25 +0530

    ssss

--- lib/ui/widgets/food_point.dart
+++ lib/ui/widgets/food_point.dart
@@ -7,6 +7,7 @@ import 'package:freeeatsin/core/provider/user_provider.dart';
 import 'package:freeeatsin/core/services/api.dart';
 import 'package:freeeatsin/ui/widgets/event_card.dart';
 import 'package:provider/provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class FoodPoint extends StatefulWidget {
   @override
@@ -14,53 +15,67 @@ class FoodPoint extends StatefulWidget {
 }
 
 class _FoodPointState extends State<FoodPoint> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<FoodPointProvider>(
         create: (_) => FoodPointProvider(),
         child: Consumer<FoodPointProvider>(
-            builder: (_, model, __) => Column(children: <Widget>[
-                  AspectRatio(
-                      aspectRatio: 16 / 7,
-                      child: Carousel(images: [
-                        Image.asset("assets/images/banner.png"),
-                        Image.asset("assets/images/banner.png")
-                      ], boxFit: BoxFit.cover, showIndicator: false)),
-                  Expanded(
-                      child: FutureBuilder(
-                          future: API.getDashboardEvents(context
-                              .read<UserProvider>()
-                              .userLoginResponseModel
-                              .message
-                              .id),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState !=
-                                ConnectionState.done) {
-                              return Center(child: CircularProgressIndicator());
-                            } else {
-                              if (snapshot.data
-                                  is DashboardEventsResponseModel) {
-                                DashboardEventsResponseModel model =
-                                    snapshot.data;
-                                if (!model.success)
-                                  return Text("Model doest return");
-                                if (model.message.length == 0)
-                                  return Center(
-                                      child: Text(
-                                          "No event available! Check back later.",
-                                          style: TextStyle(fontSize: 18.0)));
-                                return ListView.builder(
-                                    shrinkWrap: true,
-                                    itemBuilder: (context, index) => EventCard(
-                                        foodPointModel: model.message[index],
-                                        index: index,
-                                        cardType: CardType.FOOD_POINT,
-                                        isHelp: false),
-                                    itemCount: model.message.length);
-                              } else
-                                return Text("Unable to load data");
-                            }
-                          }))
-                ])));
+            builder: (_, model, __) => SmartRefresher(
+                  controller: _refreshController,
+                  enablePullDown: true,
+                  onRefresh: () {
+                    setState(() {});
+                    _refreshController.refreshCompleted();
+                  },
+                  child: Column(children: <Widget>[
+                    AspectRatio(
+                        aspectRatio: 16 / 7,
+                        child: Carousel(images: [
+                          Image.asset("assets/images/banner.png"),
+                          Image.asset("assets/images/banner.png")
+                        ], boxFit: BoxFit.cover, showIndicator: false)),
+                    Expanded(
+                        child: FutureBuilder(
+                            future: API.getDashboardEvents(context
+                                .read<UserProvider>()
+                                .userLoginResponseModel
+                                .message
+                                .id),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState !=
+                                  ConnectionState.done) {
+                                return Center(
+                                    child: CircularProgressIndicator());
+                              } else {
+                                if (snapshot.data
+                                    is DashboardEventsResponseModel) {
+                                  DashboardEventsResponseModel model =
+                                      snapshot.data;
+                                  if (!model.success)
+                                    return Text("Model doest return");
+                                  if (model.message.length == 0)
+                                    return Center(
+                                        child: Text(
+                                            "No event available! Check back later.",
+                                            style: TextStyle(fontSize: 18.0)));
+                                  return ListView.builder(
+                                      shrinkWrap: true,
+                                      itemBuilder: (context, index) =>
+                                          EventCard(
+                                              foodPointModel:
+                                                  model.message[index],
+                                              index: index,
+                                              cardType: CardType.FOOD_POINT,
+                                              isHelp: false),
+                                      itemCount: model.message.length);
+                                } else
+                                  return Text("Unable to load data");
+                              }
+                            }))
+                  ]),
+                )));
   }
 }
